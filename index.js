57c240cc8fa4ba05469069f6fe02e21945f9d17f
gourab <rgourab02@gmail.com>
2022-11-09 17:36:24 +0600

    clean check

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.m8joqcm.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
